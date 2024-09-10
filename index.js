85c4981ad757d4c3b891d6a7130938b97ee161e3
fmarcos38 <fmarcos_23@hotmail.com>
2024-09-10 11:17:17 -0300

    gcloud

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(cors());
 
 const port = process.env.PORT || 3001;
 
+app.get('/', (req, res) => {
+    res.send('Hello from Express on google!');
+});
 
 app.use('/propiedades', routerPropiedades);
 
