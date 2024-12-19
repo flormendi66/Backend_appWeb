554a67d61b333b0ffa2f2d9425a792973cd1a764
fmarcos38 <fmarcos_23@hotmail.com>
2024-12-19 14:18:50 -0300

    paginacion

--- src/Controlers/propiedades.js
+++ src/Controlers/propiedades.js
@@ -10,6 +10,7 @@ const { normalizaProps, normalizoPropiedad } = require('../Helpers/normalizaProp
     "previous": null,
     "total_count": 5
 },
+
 */
 
 const apiKey = process.env.API_KEY;
@@ -17,15 +18,17 @@ const url = process.env.URL;
 
 //trae propiedades
 const getProperties = async(req, res) => { 
-    const {limit, offset, operacion, tipo, precioMin, precioMax} = req.query; console.log("req.query:",req.query);
+    const {limit, offset, operacion, tipo, precioMin, precioMax} = req.query; console.log("data:", req.query)
+    
     try {
         let resp;
         let total; //total = resp.data.meta.total_count;
         let propiedades;
 
-        resp = await axios.get(`${url}&key=${apiKey}`);        
-        //normalizo data q me llega
-        
+        //traigo data de la api
+        resp = await axios.get(`${url}&limit=${limit}&offset=${offset}&key=${apiKey}`);        
+        total = resp.data.meta.total_count;
+        //normalizo data q me llega        
         propiedades = normalizaProps(resp.data.objects);//puedo tener 20 props como max
 
         // Filtros
@@ -57,21 +60,10 @@ const getProperties = async(req, res) => {
                 )
             );
         }        
-        
-        total = propiedades.length;
-        
-        let propsPaginadas = [];
-        let start = Number(offset); // Índice de inicio basado en el offset
-        let end = start + Number(limit); // Define el rango correcto hasta limit
-
-        // Asegúrate de que no se exceda el tamaño de las propiedades
-        for (let i = start; i < end && i < propiedades.length; i++) {
-            propsPaginadas.push(propiedades[i]);
-        }
 
         res.json({
             total,
-            propiedades: propsPaginadas,
+            propiedades,
         });
     } catch (error) {
         console.log(error);
